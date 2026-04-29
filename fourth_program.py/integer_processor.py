d10b3acb2254d737471caff01786d7fb533f9c7c
ivanmula-1 <ivanmula74@gmail.com>
2026-04-29 16:38:46 +0800

    Write squares of even numbers to double.txt
    Add functionality to write squares of even numbers to double.txt

--- fourth_program.py/integer_processor.py
+++ fourth_program.py/integer_processor.py
@@ -4,3 +4,8 @@ def process_integers(input_filename):
             numbers = [int(line.strip()) for line in source_file if line.strip()]
 
 with open('double.txt', 'w') as double_file, open('triple.txt', 'w') as triple_file:
+
+     for num in numbers:
+                if num % 2 == 0:
+                    square = num ** 2
+                    double_file.write(f"{square}\n")
