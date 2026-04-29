daac1f7cce49f76e663561d6d6769adbc407574b
ivanmula-1 <ivanmula74@gmail.com>
2026-04-29 15:28:01 +0800

    Add functionality to separate odd and even numbers

--- first_program.py/odd_and_even.py
+++ first_program.py/odd_and_even.py
@@ -3,3 +3,6 @@ def split_numbers(input_file):
         with open(input_file, 'r') as f:
 
             numbers = [int(line.strip()) for line in f if line.strip()]
+
+evens = [str(n) for n in numbers if n % 2 == 0]
+        odds = [str(n) for n in numbers if n % 2 != 0]
